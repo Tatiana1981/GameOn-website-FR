76f7016251654d6776e6ab8f081425da92c88ff1
opsonet <luc.leonardi@opsonet.com>
2025-01-09 10:35:41 +0100

    format with prettier

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,7 +8,7 @@ function editNav() {
 }
 
 function validate() {
-  console.log('validate!')
+  console.log("validate!");
 }
 
 // DOM Elements
@@ -16,23 +16,23 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const form = document.querySelector('form[name="reserve"]');
-const burger = document.querySelector('#burger');
+const burger = document.querySelector("#burger");
 
-form.addEventListener('submit', function(e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   const f = e.target;
-  console.log('target', f);
+  console.log("target", f);
   const formData = new FormData(f);
   let isError = false;
-  console.log('submit form reserve', formData);
-  if (formData.get('first').length < 3) {
-    const first = document.getElementById('first');
-    first.closest('.formData').dataset.errorVisible = true;
+  console.log("submit form reserve", formData);
+  if (formData.get("first").length < 3) {
+    const first = document.getElementById("first");
+    first.closest(".formData").dataset.errorVisible = true;
     isError = true;
   }
-  if (formData.get('last').length < 3) {
-    const last = document.getElementById('last');
-    last.closest('.formData').dataset.errorVisible = true;
+  if (formData.get("last").length < 3) {
+    const last = document.getElementById("last");
+    last.closest(".formData").dataset.errorVisible = true;
     isError = true;
   }
 
@@ -42,11 +42,11 @@ form.addEventListener('submit', function(e) {
   validate();
 });
 
-burger.addEventListener('click', function(e) {
+burger.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log('click burger');
+  console.log("click burger");
   editNav();
-})
+});
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -54,6 +54,5 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  console.log("launch Modal");
 }
-
-
