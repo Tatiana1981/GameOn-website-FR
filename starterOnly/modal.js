7d8a1950326add73a4901c568dccb2c56eff452e
Tatiana1981 <tatyumi29@gmail.com>
2025-01-13 10:42:21 +0100

    validation formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -18,8 +18,9 @@ const formData = document.querySelectorAll(".formData");
 const form = document.querySelector('form[name="reserve"]');
 const burger = document.querySelector('#burger');
 const closeBtn = document.querySelector(".close-btn");
-const email = document.querySelectorAll(".email");
-const birthdate = document.querySelector(".birthdate");
+const email = document.querySelector('input[name="email"]');
+const birthdate = document.querySelector('input[name="birthdate"]');
+const quantity = document.querySelector('input[name="quantity"]');
 
 // Close Btn
 closeBtn.addEventListener('click', function(e) {
@@ -34,17 +35,33 @@ form.addEventListener("submit", function (e) {
   const f = e.target;
   console.log("target", f);
   const formData = new FormData(f);
+  console.log(formData.get("location"));
+  if (formData.get("location") === null) {
+    const location = document.getElementById("location1");
+    location.closest(".formData").dataset.errorVisible = true;
+  }
+  else {
+    const location = document.getElementById("location1");
+    location.closest(".formData").dataset.errorVisible = false;
+  }
   let isError = false;
   console.log("submit form reserve", formData);
-  if (formData.get("first").length < 3) {
+  if (formData.get("first").length < 2) {
     const first = document.getElementById("first");
     first.closest(".formData").dataset.errorVisible = true;
     isError = true;
   }
+  else {
+    const first = document.getElementById("first");
+    first.closest(".formData").dataset.errorVisible= false;}
   if (formData.get("last").length < 3) {
     const last = document.getElementById("last");
     last.closest(".formData").dataset.errorVisible = true;
     isError = true;
+  }
+  else {
+    const last = document.getElementById("last");
+    last.closest(".formData").dataset.errorVisible = false;
   }
 
   if (isError) {
@@ -54,16 +71,18 @@ form.addEventListener("submit", function (e) {
 });
 
 // email validation
-email.forEach((e) => {
-  e.addEventListener("change", function (e) {
+  email.addEventListener("change", function (e) {
     console.log("change email", e.target.value);
+    const email = document.getElementById("email");
     if (!e.target.checkValidity()) {
+      const email = document.getElementById("email");
       e.target.closest(".formData").dataset.errorVisible = true;
     } else {
+      const email = document.getElementById("email");
       e.target.closest(".formData").dataset.errorVisible = false;
     }
   });
-});
+
 
 //date of birth validation
 birthdate.addEventListener("change", function (e) {
@@ -79,7 +98,15 @@ birthdate.addEventListener("change", function (e) {
   }
 });
 
-
+//nombre de concours
+quantity.addEventListener("change", function (e) {
+  console.log("change quantity", e.target.value);
+  if (e.target.value < 0 || e.target.value > 99) {
+    e.target.closest(".formData").dataset.errorVisible = true;
+  } else {
+    e.target.closest(".formData").dataset.errorVisible = false;
+  }
+});
 
 // Burger event
 burger.addEventListener("click", function (e) {
